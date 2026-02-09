093f33609bdda82faac8e774734117bc626fd228
gis1030 <geodata.gis1030@gmail.com>
2026-02-09 10:56:02 +0100

    Enterprises registered with the BCE located in the municipality of Schaerbeek (Taxe08)

---
+++ BCE1030/js/index_controls_BCE.js
@@ -0,0 +1,210 @@
+// ++++++++ Choix de cartes et Elements ++++++++
+var CarteGroup = {
+    "<b>OSM.fr Carte</b>": carte01,
+    "<b>Google Terrain</b>": carte02,
+    /* "<b>Google Streets</b>": carte03,
+     "<b>Google Satellite</b>": carte04,
+     "<b>Google Satellite-Streets</b>": carte05,
+     "<b>Google Terrain-Cycle</b>": carte06,
+     "<b>Google Streets-Cycle</b>": carte07,
+     "<b>Google Traffic</b>": carte08,*/
+};
+
+var GroupDataALL = {
+    "<b>agriculture, sylviculture et pêche</b></br>": GroupMarkersMap1030_Q01,
+    "<b>industries extractives</b></br>": GroupMarkersMap1030_Q02,
+    "<b>industrie manufacturière</b></br>": GroupMarkersMap1030_Q03,
+    "<b>production et distribution d’électricité, de gaz, de vapeur et d’air conditionné</b></br>": GroupMarkersMap1030_Q04,
+    "<b>production et distribution d’eau; assainissement, gestion des déchets et dépollution</b></br>": GroupMarkersMap1030_Q05,
+    "<b>construction</b></br>": GroupMarkersMap1030_Q06,
+    "<b>commerce</b></br>": GroupMarkersMap1030_Q07,
+    "<b>transports et entreposage</b></br>": GroupMarkersMap1030_Q08,
+    "<b>hébergement et restauration</b></br>": GroupMarkersMap1030_Q09,
+    "<b>édition, diffusion et activités de production et de distribution de contenu</b></br>": GroupMarkersMap1030_Q10,
+    "<b>télécommunications, programmation informatique, conseil, infrastructure informatique et autres activités de service d'information</b></br>": GroupMarkersMap1030_Q11,
+    "<b>activités financières et d’assurance</b></br>": GroupMarkersMap1030_Q12,
+    "<b>activités immobilières</b></br>": GroupMarkersMap1030_Q13,
+    "<b>activités spécialisées, scientifiques et techniques</b></br>": GroupMarkersMap1030_Q14,
+    "<b>activités de service administratif et de soutien</b></br>": GroupMarkersMap1030_Q15,
+    "<b>enseignement</b></br>": GroupMarkersMap1030_Q16,
+    "<b>santé humaine et activités d’action sociale</b></br>": GroupMarkersMap1030_Q17,
+    "<b>arts, sports et activités récréatives</b></br>": GroupMarkersMap1030_Q18,
+    "<b>autres activités de services</b></br>": GroupMarkersMap1030_Q19,
+    "<b>administration publique et défense; sécurité sociale obligatoire</b></br>": GroupMarkersMap1030_Q20,
+    "<b>activités des ménages en tant qu’employeurs; activités indifférenciées des ménages en tant que producteurs de biens et services pour usage propre</b></br>": GroupMarkersMap1030_Q21,
+    "<b>activités des organisations et organismes extraterritoriaux</b></br>": GroupMarkersMap1030_Q22,
+    "<b>non renseigné</b></br>": GroupMarkersMap1030_Q22,
+    "<b>toutes les entites</b></br>": GroupMarkersMap1030_00,
+};
+
+var GroupDataALL_Data = {
+    "<b>toutes les entites</b></br>": GroupMarkersMap1030_00
+};
+
+var GroupDataALL_Division = {
+    "<b>commerce de gros</b></br>": GroupMarkersMap1030_D46,
+    "<b>commerce de détail</b></br>": GroupMarkersMap1030_D47,
+    "<b>hébergement</b></br>": GroupMarkersMap1030_D55,
+    "<b>activités de service de restauration</b></br>": GroupMarkersMap1030_D56,
+    "<b>autres division NACE2025</b></br>": GroupMarkersMap1030_D99,
+
+    "<b>non renseigné</b></br>": GroupMarkersMap1030_D00,
+    "<b>toutes les entites</b></br>": GroupMarkersMap1030_00
+};
+
+var GroupDataALL_ONYX = {
+    "<b>enregistré sur onyx</b></br>": GroupMarkersMap1030_C01,
+    "<b>non enregistré sur onyx</b></br>": GroupMarkersMap1030_C00,
+    "<b>toutes les entites</b></br>": GroupMarkersMap1030_00
+};
+
+//var LControl01 = L.control.layers(GroupDataALL).addTo(carte);
+var LControl01 = L.control.layers(GroupDataALL_Division).addTo(carte);
+var LControl02 = L.control.layers(GroupDataALL_ONYX).addTo(carte);
+var LControl00 = L.control.layers(CarteGroup).addTo(carte);
+// ++++++++ Choix de cartes et Elements ++++++++
+
+// +++++++ Revient a la position initial ++++++++
+L.easyButton('<img src="images/home.png" width="26" height="26" >', function () {
+    carte.setView([50.86070401717095, 4.3830651582691456], 13);
+    layerGroup.clearLayers();
+}).addTo(carte);
+
+// +++++++ Géolocalisation ++++++++
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
+    var location = e.latlng
+    L.marker(location).addTo(carte)
+    L.circle(location, radius).addTo(carte);
+}
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+function getLocationLeafvar() {
+    carte.on('locationfound', onLocationFound);
+    carte.on('locationerror', onLocationError);
+    carte.locate({ setView: true, maxZoom: 15 });
+}
+
+L.easyButton('<img src="images/gps-position.png" width="26" height="26" >', function () {
+    getLocationLeafvar()
+}).addTo(carte);
+
+// +++++++  MousePosition Maps ++++++++
+var mousePoistion = L.geoportalControl.MousePosition({
+    position: 'bottomright',
+    collapsed: true,
+    units: [],
+}).addTo(carte);
+
+// +++++++ Esri Leafvar Geocoder ++++++++
+var searchControl = L.esri.Geocoding.geosearch({
+    position: 'topleft',
+    zoomToResult: true,
+    useMapBounds: true,
+    providers: [L.esri.Geocoding.arcgisOnlineProvider()],
+    collapseAfterResult: true,
+    expanded: false,
+    title: 'Recherche d`emplacement',
+    placeholder: 'SVP, Entrez une adresse...'
+}).addTo(carte);
+
+var geocodeService = L.esri.Geocoding.geocodeService({});
+var layerGroup = L.layerGroup().addTo(carte);
+carte.on('click', function (e) {
+
+    geocodeService.reverse().latlng(e.latlng).run(function (error, result) {
+        if (error) {
+            return;
+        }
+        //var emplacement = convertToAddress(e.latlng);
+        var lngLatString = `${Math.round(result.latlng.lat * 100000) / 100000}, ${Math.round(result.latlng.lng * 100000) / 100000}`;
+        layerGroup.clearLayers();
+        marker = L.marker(result.latlng)
+            .addTo(layerGroup) //.addTo(carte) l'utilisation de vette option permet au marqueur de rester sur la carte
+            .bindPopup(`<dt>${lngLatString}</dt>` + `<dt>${result.address.LongLabel}</dt>`) // adresse avec les coordonnées gps 
+            //.bindPopup(result.address.Match_addr) // adresse sans les coordonnées gps 
+            //.bindPopup(result.address.LongLabel) // version plus longue de l'adresse Match_addr contenant plus d'informations administratives
+            //.bindPopup(result.address.ShortLabel) // version abrégée de l'adresse Match_addr
+            .openPopup();
+    });
+});
+
+// +++++++ menu principal +++++++++++++++++
+let MyControlClass = L.Control.extend({
+    options: {
+        position: 'topleft',
+    },
+
+    onAdd: function (carte) {
+        var div = L.DomUtil.create('div', 'leaflet-bar my-control');
+
+        var myButton = L.DomUtil.create('button', 'my-button-class', div);
+        myButton.innerHTML = 'menu principal';
+        myButton.style.width = '85px';
+        L.DomEvent.on(myButton, 'click', function () {
+            window.open("../index.html");
+            window.close();
+        }, this);
+        return div;
+    },
+
+    onRemove: function (carte) {
+    }
+});
+let myControl = new MyControlClass().addTo(carte);
+
+// +++++++ Menu principal  +++++++++++
+/*
+let ChartPopClass = L.Control.extend({
+    options: { position: 'topleft' },
+
+    onAdd: function (carte) {
+        var div = L.DomUtil.create('div', 'leaflet-bar my-control');
+
+        var myButton = L.DomUtil.create('button', 'my-button-class', div);
+        myButton.innerHTML = 'Affichage des statistiques globales';
+        myButton.style.width = '150px';
+        myButton.style.height = '30px';
+        L.DomEvent.on(myButton, 'click', function () {
+            window.open("BCE1030Charts.html");
+            window.close();
+        }, this);
+        return div;
+    },
+
+    onRemove: function (carte) {
+    }
+});
+let ChartPopControl = new ChartPopClass().addTo(carte);
+*/
+
+// +++++++ Title  +++++++++++
+var title = new L.Control({ 'position': 'bottomleft' });
+title.onAdd = function (carte) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+title.update = function () {
+    this._div.innerHTML = '<a style="font-size: 17px; background-color: white; border: 4px solid white">Entités commerciales BCE-1030</a>';
+};
+title.addTo(carte);
+
+// +++++++ .Watermark  ++++++++++
+L.Control.Watermark = L.Control.extend({
+    onAdd: function (carte) {
+        var img = L.DomUtil.create('img');
+        img.src = 'images/schaerbeek1030_logo_white.png';
+        img.style.width = '75px';
+
+        return img;
+    },
+    onRemove: function (carte) {
+        // Nothing to do here
+    }
+});
+L.control.watermark = function (opts) { return new L.Control.Watermark(opts); }
+L.control.watermark({ position: 'bottomleft' }).addTo(carte);
